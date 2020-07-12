0da443d28f860387abba337c7c8ef93163e335b9
Sheiyla <sheiylanie@ovh.fr>
2020-07-13 00:13:16 +0200

    Add Pub Doors

---
+++ Doors/Pub.cs
@@ -0,0 +1,22 @@
+﻿using Onsharp.Events;
+using Onsharp.World;
+
+namespace OnSharp_Doors
+{
+
+	public class Pub : OnSharp_Doors
+	{
+
+		[ServerEvent(EventType.PackageStart)]
+		public void OnPub()
+		{
+
+			// Desert Town 2 Pub
+			Server.CreateDoor(56, new Vector(48671.000000, 133162.000000, 1477.000000), 90.0, true);
+			Server.CreateDoor(57, new Vector(48671.000000, 133404.000000, 1477.000000), 90.0, true);
+
+		}
+
+	}
+
+}
